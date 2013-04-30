e8d8724a8e0513340f475cfbd70a88888d466e62
Seth <seth@internet-inspired.com>
2013-04-30 20:32:48 +0100

    Say m'eh to bloat
    I'll kill dat whitespace later!

--- index.php
+++ index.php
@@ -1,190 +1,139 @@
 <?php defined('_JEXEC') or die;
 /* =====================================================================
-Template:	OneWeb for Joomla 2.5
+Template:	OneWeb for Joomla
 Author: 	Seth Warburton - Internet Inspired! - @nternetinspired
-Version: 	2.0
-Created: 	June 2012
-Copyright:	Seth Warburton - (C) 2012 - All rights reserved
-Licenses:	GNU/GPL v2 or later http://www.gnu.org/licenses/gpl-2.0.html DBAD License http://philsturgeon.co.uk/code/dbad-license
-Sources:	http://gangnamboilerplate.com/ http://construct-framework.com/
+Version: 	3.0
+Created: 	April 2013
+Copyright:	Seth Warburton - (C) 2013 - All rights reserved
+Licenses:	GNU/GPL v3 or later http://www.gnu.org/licenses/gpl-2.0.html
+            DBAD License http://philsturgeon.co.uk/code/dbad-license
 /* ===================================================================== */
 
 // Load template logic
 include_once JPATH_THEMES . '/' . $this->template . '/logic.php';
 ?>
 <!doctype html>
-<!-- gangnam Boilerplate -->
-<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
-<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
-<!--[if IE 7]> <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
-<!--[if IE 8]> <html class="no-js lt-ie9" lang="en"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 <head>
 <jdoc:include type="head" />
+    <!--[if lt IE 9]>
+        <script src="<?php echo $this->baseurl ?>/media/jui/js/html5.js"></script>
+    <![endif]-->
 </head>
-<body class="<?php echo $pageParams->get('pageclass_sfx'); ?> clearfix">
-<div id="buttonRow" class="mobile clearfix">
-  <?php if($this->countModules('search')) : ?>
-		<a id="mobileSearch" href="#search" title="Jump to search">Search</a>
-  <?php endif; ?>
-  <a id="mobileMenu" href="#menu" title="Jump to navigation">Menu</a>
-</div>
-<div id="logoRow" class="row clearfix">
-	<a href="<?php echo $this->baseurl ?>/" title="<?php echo htmlspecialchars($app->getCfg('sitename'));?>">
-        <header id="logo"><?php echo htmlspecialchars($app->getCfg('sitename'));?></header>
-    </a>
-</div>
-<div id="container">
-	<?php if($this->countModules('banner')) : ?>
-    <div id="bannerRow" class="row clearfix">
-        <header role="banner" id="banner">
-            <jdoc:include type="modules" name="banner" style="gangnam" headerLevel="4"/>
-        </header>
-    </div>
-    <?php endif; ?>
-    <?php if ($aboveModules > 0) : ?>
-	<div id="aboveRow" class="row clearfix">
-		<?php if($this->countModules('above1')) : ?>
-        <div id="above1">
-                <jdoc:include type="modules" name="above1" style="gangnam" headerLevel="4"/>
+    <body class="<?php echo $siteHome ; ?>-page <?php echo $option . " view-" . $view . " itemid-" . $itemid . "";?>">
+
+        <div class="header-row">
+            <header role="banner">
+                <div class="logo">
+                    <a href="<?php echo $this->baseurl ?>/" title="<?php echo htmlspecialchars($app->getCfg('sitename'));?>"><?php echo htmlspecialchars($app->getCfg('sitename'));?></a>
+                </div>
+                <?php if($this->countModules('menu')) : ?>
+                    <nav role="navigation">
+                        <jdoc:include type="modules" name="menu" style="gangnam" />
+                    </nav>
+                <?php endif; ?>
+            </header>
         </div>
-        <?php endif; ?>
-        <?php if($this->countModules('above2')) : ?>
-        <div id="above2">
-                <jdoc:include type="modules" name="above2" style="gangnam" headerLevel="4"/>
-        </div>
-        <?php endif; ?>
-        <?php if($this->countModules('above3')) : ?>
-        <div id="above3">
-                <jdoc:include type="modules" name="above3" style="gangnam" headerLevel="4"/>
-        </div>
-        <?php endif; ?>
-        <?php if($this->countModules('above4')) : ?>
-        <div id="above4">
-                <jdoc:include type="modules" name="above4" style="gangnam" headerLevel="4"/>
-        </div>
-        <?php endif; ?>
-        </div>
-	<?php endif; ?>
-    <?php if($this->countModules('breadcrumbs')) : ?>
-    <div id="breadcrumbRow" class="row clearfix">
-        <nav id="breadcrumbs" role="navigation">
-            <jdoc:include type="modules" name="breadcrumbs" style="gangnam" headerLevel="4"/>
-        </nav>
-    </div>
-    <?php endif; ?>
-    <?php if($siteHome == 'sub' or ($frontpage == 0)) : ?>
-    <div id="mainRow" class="row clearfix">
-        <?php if($this->countModules('left') or $this->countModules('search')) : ?>
-        <div id="main" role="main" class="mainLeft last clearfix">
-            <jdoc:include type="message" />
-            <jdoc:include type="component" />
-        </div>
-        <aside id="sidebar" class="clearfix" role="complementary">
-		  <?php if($this->countModules('left')) : ?>
-          <div id="left" class="clearfix">
-                  <jdoc:include type="modules" name="left" style="gangnam"/>
-          </div>
-          <?php endif; ?>
-          <?php if($this->countModules('search')) : ?>
-            <div id="search" role="search">
-                  <jdoc:include type="modules" name="search" style="gangnam" />
+
+        <?php if($this->countModules('banner')) : ?>
+            <div class="banner-row">
+                <section>
+                    <jdoc:include type="modules" name="banner" style="gangnam" />
+                </section>
             </div>
-          <?php endif; ?>
-        </aside>
-        <?php else : ?>
-        <div id="main" role="main" class="main last clearfix">
-        <jdoc:include type="message" />
-        <jdoc:include type="component" />
-        </div>
         <?php endif; ?>
-    </div>
-    <?php endif; ?>
-    <nav id="menu" role="navigation" class="row clearfix"> <!-- main nav is here for usability and SEO reasons -->
-    	<jdoc:include type="modules" name="menu" style="gangnam" />
-    </nav>
-    <?php if ($bottomModules > 0) : ?>
-    <div id="bottomRow" class="row clearfix">
-        <?php if($this->countModules('bottom1')) : ?>
-        <div id="bottom1">
-              <jdoc:include type="modules" name="bottom1" style="gangnam" />
-        </div>
+
+        <?php if($this->countModules('above')) : ?>
+            <div class="above-row">
+                <section>
+                   <jdoc:include type="modules" name="above" style="gangnam" />
+                </section>
+            </div>
         <?php endif; ?>
-        <?php if($this->countModules('bottom2')) : ?>
-        <div id="bottom2">
-              <jdoc:include type="modules" name="bottom2" style="gangnam" />
+
+        <div class="main-row">
+            <section>
+                <main role="main">
+                    <jdoc:include type="message" />
+                    <jdoc:include type="component" />
+                </main>
+                <?php if($this->countModules('complementary')) : ?>
+                <div role="complementary">
+                    <jdoc:include type="modules" name="complementary" style="gangnam" />
+                </div>
+                <?php endif; ?>
+            </section>
         </div>
+
+        <?php if($this->countModules('below')) : ?>
+            <div class="below-row">
+                <section>
+                    <jdoc:include type="modules" name="below" style="gangnam" />
+                </section>
+            </div>
         <?php endif; ?>
-        <?php if($this->countModules('bottom3')) : ?>
-        <div id="bottom3">
-              <jdoc:include type="modules" name="bottom3" style="gangnam" />
+
+        <?php if($this->countModules('footer-menu') OR $this->countModules('content-info')) : ?>
+            <div class="footer-row">
+                    <footer role="contentinfo">
+                        <?php if($this->countModules('footer-menu')) : ?>
+                            <nav class="footer-menu">
+                                <jdoc:include type="modules" name="footer-menu" style="gangnam" />
+                            </nav>
+                        <?php endif; ?>
+                        <?php if($this->countModules('contentinfo')) : ?>
+                            <jdoc:include type="modules" name="content-info" style="gangnam" />
+                        <?php endif; ?>
+                    </footer>
+                </div>
+        <?php endif; ?>
+
+        <div class="credit-row">
+            <footer>
+                <?php if ($social > 0) : ?>
+                    <ul class="social">
+                        <?php if ($twitterLink != "") : ?>
+                            <li><a class="twitter" href="<?php echo ($twitterLink); ?>" title="Follow me on Twitter" target="_blank"><?php echo ($twitter); ?></a></li>
+                        <?php endif; ?>
+                        <?php if ($dribbbleLink != "") : ?>
+                            <li><a class="dribbble" href="<?php echo ($dribbbleLink); ?>" title="See my latest work at Dribbble" target="_blank"><?php echo ($dribbble); ?></a></li>
+                        <?php endif; ?>
+                        <?php if ($facebookLink != "") : ?>
+                            <li><a class="facebook" href="<?php echo ($facebookLink); ?>" title="Annoy me on Facebook" target="_blank"><?php echo ($facebook); ?></a></li>
+                        <?php endif; ?>
+                        <?php if ($googleplusLink != "") : ?>
+                            <li><a class="googleplus" href="<?php echo ($googleplusLink); ?>" title="Find me on G+" target="_blank"><?php echo ($tgoogleplus); ?></a></li>
+                        <?php endif; ?>
+                        <?php if ($githubLink != "") : ?>
+                            <li><a class="github" href="<?php echo ($githubLink); ?>" title="All the codez" target="_blank"><?php echo ($github); ?></a></li>
+                        <?php endif; ?>
+                    </ul>
+                <?php endif; ?>
+                <div class="copyright">
+                    <small>&copy; <?php echo date("Y"); ?> <?php echo htmlspecialchars($app->getCfg('sitename'));?></small>
+                </div>
+            </footer>
         </div>
+
+        <?php if($this->countModules('debug')) : ?>
+            <jdoc:include type="modules" name="debug"/>
         <?php endif; ?>
-        <?php if($this->countModules('bottom4')) : ?>
-        <div id="bottom4">
-              <jdoc:include type="modules" name="bottom4" style="gangnam" />
-        </div>
+
+        <?php if ($scripts > 0) : ?>
+        	<script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/js/scripts.js"></script>
+            <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/js/plugins.js"></script>
         <?php endif; ?>
-            <?php if($this->countModules('bottom5')) : ?>
-        <div id="bottom5">
-              <jdoc:include type="modules" name="bottom5" style="gangnam" />
-        </div>
+        <?php if ($analytics != "") : ?>
+            <script>
+                var _gaq=[["_setAccount","<?php echo htmlspecialchars($analytics); ?>"],["_trackPageview"]];
+                (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
+                g.src=("https:"==location.protocol?"//ssl":"//www")+".google-analytics.com/ga.js";
+                s.parentNode.insertBefore(g,s)}(document,"script"));
+            </script>
         <?php endif; ?>
-        <?php if($this->countModules('bottom6')) : ?>
-        <div id="bottom6">
-              <jdoc:include type="modules" name="bottom6" style="gangnam" />
-        </div>
-        <?php endif; ?>
-    </div>
-    <?php endif; ?>
-    <?php if ($footerModules > 0) : ?>
-    <footer role="contentinfo" class="row clearfix">
-        <?php if($this->countModules('footer1')) : ?>
-        <div id="footer1">
-            <jdoc:include type="modules" name="footer1" style="gangnam" headerLevel="4" />
-        </div>
-        <?php endif; ?>
-        <?php if($this->countModules('footer2')) : ?>
-        <div id="footer2">
-            <jdoc:include type="modules" name="footer2" style="gangnam" headerLevel="4" />
-        </div>
-        <?php endif; ?>
-        <?php if($this->countModules('footer3')) : ?>
-        <div id="footer3">
-            <jdoc:include type="modules" name="footer3" style="gangnam" headerLevel="4" />
-        </div>
-        <?php endif; ?>
-        <?php if($this->countModules('footer4')) : ?>
-        <div id="footer4">
-            <jdoc:include type="modules" name="footer4" style="gangnam" headerLevel="4"/>
-        </div>
-        <?php endif; ?>
-    </footer>
-	<?php endif; ?>
-</div>
-<footer id="creditRow" class="row clearfix">
-    <div id="creditRight"><a href="http://joomlafuture.com" title="OneWeb, a free, responsive, gangnam template for Joomla! 2.5+">OneWeb</a>, a free Joomla! template from <a href="http://internet-inspired.com" title="Internet Inspired - Professional Joomla! Websmithing">Internet Inspired!</a> &copy; <?php echo date("Y"); ?></div>
-</footer>
-<jdoc:include type="modules" name="debug"/>
-<?php if ($scripts > 0) : ?>
-<!-- Custom template scripts. Place your plugin code and script calls in the following two file and enable custom script loading in the template settings -->
-<script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/js/scripts.js"></script>
-<script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/js/plugins.js"></script>
-<?php endif; ?>
-<script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/js/helper.js"></script>
-<!-- iOS scale bug fix -->
-<script>
-MBP.scaleFix();
-</script>
-<?php if ($analytics != "UA-XXXXX-X") : ?>
-<!-- http://mths.be/aab -->
-<script>
-var _gaq=[['_setAccount','<?php echo htmlspecialchars($analytics); ?>'],["_trackPageview"]];
-(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
-g.src=("https:"==location.protocol?"//ssl":"//www")+".google-analytics.com/ga.js";
-s.parentNode.insertBefore(g,s)}(document,"script"));
-</script>
-<?php endif; ?>
-<noscript>JavaScript is unavailable or disabled; so you are probably going to miss out on a few things. Everything should still work, but with a little less pzazz!</noscript>
-</body>
+
+    </body>
 </html>
