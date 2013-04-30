9d653395f5fc3209d2d33ffe4fad577ad3329aeb
Seth <seth@internet-inspired.com>
2013-04-30 21:00:30 +0100

    but it just won't go away

--- js/plugins.js
+++ js/plugins.js
@@ -15,6 +15,62 @@ window.log = function(){
 (function(b){function c(){}for(var d="assert,clear,count,debug,dir,dirxml,error,exception,firebug,group,groupCollapsed,groupEnd,info,log,memoryProfile,memoryProfileEnd,profile,profileEnd,table,time,timeEnd,timeStamp,trace,warn".split(","),a;a=d.pop();){b[a]=b[a]||c}})((function(){try
 {console.log();return window.console;}catch(err){return window.console={};}})());
 
-
 // place any jQuery/helper plugins in here, instead of separate, slower script files.
 
+/*!
+* OneWeb v3.0
+* Author: Seth Warburton
+* Copyright: Seth Warburton - (C) 2013 - All rights reserved
+* Licenses: GNU/GPL v2 or later http://www.gnu.org/licenses/gpl-2.0.html
+*           DBAD License http://philsturgeon.co.uk/code/dbad-license
+* Date: 09 January 2013
+*/
+
+(function( $ ) {
+  $.fn.deviceHooks = function() {
+
+      var resizer = function() {
+
+          if ($(window).width() > 319) {
+              $("html").removeClass("device-dumb").removeClass("device-lap").addClass("device-palm");
+          }
+
+          if ($(window).width() > 599) {
+              $("html").removeClass("device-palm").removeClass("device-portable").addClass("device-lap");
+          }
+
+          if ($(window).width() > 767) {
+              $("html").removeClass("device-lap").removeClass("device-desk").addClass("device-portable");
+          }
+
+          if ($(window).width() > 991) {
+              $("html").removeClass("device-portable").removeClass("device-desk-wide").addClass("device-desk");
+          }
+
+          if ($(window).width() > 1381) {
+              $("html").removeClass("device-desk").addClass("device-desk-wide");
+          }
+
+      };
+
+      // Call once to set.
+      resizer();
+
+      // Function for testing touch screens
+     function is_touch_device() {
+         return !! ('ontouchstart' in window);
+      }
+
+      // Set class on html element for touch/no-touch
+      if (is_touch_device()) {
+         $('html').addClass('touch');
+      } else {
+          $('html').addClass('no-touch');
+      }
+
+      // Call on resize.
+      $(window).on('resize', resizer);
+
+  };
+
+})(jQuery);
--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,52 @@
-/* Author:
-Seth Warburton | @nternetinspired
+/*!
+* OneWeb v3.0
+* Author: Seth Warburton
+* Copyright: Seth Warburton - (C) 2013 - All rights reserved
+* Licenses: GNU/GPL v2 or later http://www.gnu.org/licenses/gpl-2.0.html
+*           DBAD License http://philsturgeon.co.uk/code/dbad-license
+* Date: 09 January 2013
 */
+
+// Append some styling hooks related to device capabilities to html and body elements
+// Got js?
+jQuery("html").removeClass("no-js").addClass("js-enabled");
 
 jQuery(document).ready(function(){
 
-	// Prevent iOS from zooming onfocus
-	// http://nerd.vasilis.nl/prevent-ios-from-zooming-onfocus/
-	
-	MBP.viewportMeta = jQuery('meta[name="viewport"]');
-	MBP.preventZoom = function () {
-	  jQuery('input, select, textarea').bind('focus blur', function(event) {
-		MBP.viewportMeta.attr('content', 'width=device-width,initial-scale=1,maximum-scale=' + (event.type == 'blur' ? 10 : 1));
-	  });
-	};
+	// Touch? Screen type?
+	jQuery().deviceHooks();
 
-});
+  // Checks if article images and videos have figure tags and adds them if not
+  jQuery(function figtarget() {
+     figtag(".article-content img");
+  });
+
+  function figtag(theElement) {
+    jQuery(theElement).each(function(){
+    if (!jQuery(this).parent().is("figure")) {
+        jQuery(this).wrap("<figure>");
+    }
+    });
+  }
+
+// Scroll to top
+    jQuery(function () {
+    var scrollDiv = document.createElement("a");
+    jQuery(scrollDiv).attr("class", "to-top").appendTo("body").attr("title", "Click here to soar gracefully upwards like an eagle!").text("^");
+jQuery(".to-top").fadeOut();
+    jQuery(window).scroll(function () {
+        if (jQuery(this).scrollTop() !== 0) {
+          jQuery(".to-top").fadeIn();
+        } else {
+          jQuery(".to-top").fadeOut();
+        }
+      });
+      jQuery(".to-top").click(function () {
+        jQuery("body,html").animate({
+          scrollTop: 0
+        },
+       800);
+      });
+    });
+
+ });
