ff243ed7e361476c9a4aa9a50f7b84a514969f11
Seth <seth@internet-inspired.com>
2013-04-30 21:01:43 +0100

    What is it?

--- html/com_content/featured/default.php
+++ html/com_content/featured/default.php
@@ -1,79 +1,52 @@
 <?php
- 
- /* =====================================================================
-Template:	OneWeb for Joomla 2.5						            
-Author: 	Seth Warburton - Internet Inspired! - @nternetinspired 				            
-Version: 	2.0 											             
-Created: 	June 2012                                                    
-Copyright:	Seth Warburton - (C) 2012 - All rights reserved		
-License:	GNU/GPL v2 or later http://www.gnu.org/licenses/gpl-2.0.html
-			DBAD License http://philsturgeon.co.uk/code/dbad-license
-Source: 	J2.5.1. com_content/views/							             		
-/* ===================================================================== */
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_content
+ *
+ * @copyright   Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
 
-// no direct access
 defined('_JEXEC') or die;
+
 JHtml::addIncludePath(JPATH_COMPONENT . '/helpers');
+
+// If the page class is defined, add to class as suffix.
+// It will be a separate class if the user starts it with a space
 ?>
-<section class="blog-featured <?php echo $this->pageclass_sfx;?>">
-<?php if ( $this->params->get('show_page_heading')!=0) : ?>
-<header>
-	<h1>
-	<?php echo $this->escape($this->params->get('page_heading')); ?>
-	</h1>
-</header>    
+<div class="blog-featured<?php echo $this->pageclass_sfx;?>">
+<?php if ($this->params->get('show_page_heading') != 0) : ?>
+<header class="page-header"><h1><?php echo $this->escape($this->params->get('page_heading')); ?></h1></header>
 <?php endif; ?>
-<?php $leadingcount=0 ; ?>
 <?php if (!empty($this->lead_items)) : ?>
 <section class="leading-articles">
 	<?php foreach ($this->lead_items as &$item) : ?>
-		<article class="article <?php echo $leadingcount; ?><?php echo $item->state == 0 ? ' system-unpublished' : null; ?> clearfix">
 			<?php
 				$this->item = &$item;
 				echo $this->loadTemplate('item');
 			?>
-		</article>
-		<?php
-			$leadingcount++;
-		?>
 	<?php endforeach; ?>
 </section>
 <?php endif; ?>
-
-<?php
-	$introcount=(count($this->intro_items));
-	$counter=0;
-?>
 <?php if (!empty($this->intro_items)) : ?>
-<section class="intro-articles">
-	<?php foreach ($this->intro_items as $key => &$item) : ?>
-		<article class="article <?php echo $counter; ?> clearfix">
-			<?php
-					$this->item = &$item;
-					echo $this->loadTemplate('item');
-			?>
-		</article>
-		<?php $counter++; ?>
-	<?php endforeach; ?>
-</section>
-<?php endif; ?>
-
-<?php if (!empty($this->link_items)) : ?>
-	<section class="more-articles">
-	<?php echo $this->loadTemplate('links'); ?>
+	<section class="intro-articles">
+		<?php foreach ($this->intro_items as &$item) : ?>
+				<?php
+						$this->item = &$item;
+						echo $this->loadTemplate('item');
+				?>
+		<?php endforeach; ?>
 	</section>
 <?php endif; ?>
-
-<?php if ($this->params->def('show_pagination', 2) == 1  || ($this->params->get('show_pagination') == 2 && $this->pagination->get('pages.total') > 1)) : ?>
-	<section class="pagination">
-
+<?php if (!empty($this->link_items)) : ?>
+	<?php echo $this->loadTemplate('links'); ?>
+<?php endif; ?>
+<?php if ($this->params->def('show_pagination', 2) == 1  || ($this->params->get('show_pagination') == 2 && $this->pagination->pagesTotal > 1)) : ?>
+	<nav class="pagination">
 		<?php if ($this->params->def('show_pagination_results', 1)) : ?>
-			<p class="counter">
 				<?php echo $this->pagination->getPagesCounter(); ?>
-			</p>
 		<?php  endif; ?>
 				<?php echo $this->pagination->getPagesLinks(); ?>
-	</section>
+	</nav>
 <?php endif; ?>
-
-</section>
+</div>
