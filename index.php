17e0e30e17b188d6d8492eb5afc536aa72955002
Seth <seth@internet-inspired.com>
2013-05-09 20:37:21 +0100

    updates shiv source

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ include_once JPATH_THEMES . '/' . $this->template . '/logic.php';
 <head>
 <jdoc:include type="head" />
     <!--[if lt IE 9]>
-        <script src="<?php echo $this->baseurl ?>/media/jui/js/html5.js"></script>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"</script>
     <![endif]-->
 </head>
     <body class="<?php echo $siteHome ; ?>-page <?php echo $option . " view-" . $view . " itemid-" . $itemid . "";?>">
