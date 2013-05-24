4c790e3bc6f8b298ecaf9f8da8028641883872d2
Matt Thomas <matt@betweenbrain.com>
2013-05-24 10:46:36 -0400

    Ie 10 ignores conditionals, no need for GT

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ include_once JPATH_THEMES . '/' . $this->template . '/logic.php';
 <!--[if IE 7]> <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
 <!--[if IE 8]> <html class="no-js lt-ie9 lt-ie10" lang="en"> <![endif]-->
 <!--[if IE 9]> <html class="no-js lt-ie10" lang="en"> <![endif]-->
-<!--[if gt IE 9]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<!--[if !IE]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
 <jdoc:include type="head" />
 </head>
