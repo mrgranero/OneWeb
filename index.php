b750ef1ede0dfdf81ba5eec2b323bd7676015028
Matt Thomas <matt@betweenbrain.com>
2013-05-24 10:07:10 -0300

    Update index.php to add LT IE 9
    Yes, believe it or not, I actually needed this recently.

--- index.php
+++ index.php
@@ -17,8 +17,9 @@ include_once JPATH_THEMES . '/' . $this->template . '/logic.php';
 <!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
 <!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
 <!--[if IE 7]> <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
-<!--[if IE 8]> <html class="no-js lt-ie9" lang="en"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<!--[if IE 8]> <html class="no-js lt-ie9 lt-ie10" lang="en"> <![endif]-->
+<!--[if IE 9]> <html class="no-js lt-ie10" lang="en"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
 <jdoc:include type="head" />
 </head>
