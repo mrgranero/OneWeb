425d141769038ba2467ed2a687511b9793bb817a
Seth <seth@internet-inspired.com>
2013-05-28 21:08:12 +0100

    updates conditionals and adds a wrapper element

--- index.php
+++ index.php
@@ -13,10 +13,11 @@ Licenses:	GNU/GPL v3 or later http://www.gnu.org/licenses/gpl-3.0.html
 include_once JPATH_THEMES . '/' . $this->template . '/logic.php';
 ?>
 <!doctype html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js dumb"> <!--<![endif]-->
+<!--[if lt IE 7]> <html class="no-js lt-ie10 lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]> <html class="no-js lt-ie10 lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]> <html class="no-js lt-ie10 lt-ie9" lang="en"> <![endif]-->
+<!--[if IE 9]> <html class="no-js lt-ie10" lang="en"> <![endif]-->
+<!--[if !IE]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
 <jdoc:include type="head" />
     <!--[if lt IE 9]>
@@ -26,46 +27,54 @@ include_once JPATH_THEMES . '/' . $this->template . '/logic.php';
     <body class="<?php echo $siteHome ; ?>-page <?php echo $option . " view-" . $view . " itemid-" . $itemid . "";?>">
 
         <div class="header-row">
-            <header role="banner">
-                <div class="logo">
-                    <a href="<?php echo $this->baseurl ?>/" title="<?php echo htmlspecialchars($app->getCfg('sitename'));?>"><?php echo htmlspecialchars($app->getCfg('sitename'));?></a>
-                </div>
-                <?php if($this->countModules('menu')) : ?>
-                    <nav role="navigation">
-                        <jdoc:include type="modules" name="menu" style="gangnam" />
-                    </nav>
-                <?php endif; ?>
-            </header>
+            <div class="wrapper">
+                <header role="banner" class="content">
+                    <div class="logo">
+                        <a href="<?php echo $this->baseurl ?>/" title="<?php echo htmlspecialchars($app->getCfg('sitename'));?>"><?php echo htmlspecialchars($app->getCfg('sitename'));?></a>
+                    </div>
+                    <?php if($this->countModules('menu')) : ?>
+                        <nav role="navigation">
+                            <jdoc:include type="modules" name="menu" style="gangnam" />
+                        </nav>
+                    <?php endif; ?>
+                </header>
+            </div>
         </div>
 
         <?php if($this->countModules('banner')) : ?>
             <div class="banner-row">
-                <section>
-                    <jdoc:include type="modules" name="banner" style="gangnam" />
-                </section>
+                <div class="wrapper">
+                    <div class="content">
+                        <jdoc:include type="modules" name="banner" style="gangnam" />
+                    </div>
+                </div>
             </div>
         <?php endif; ?>
 
         <?php if($this->countModules('breadcrumbs')) : ?>
             <div class="breadcrumb-row">
-                <section>
-                   <jdoc:include type="modules" name="breadcrumbs" style="gangnam" />
-                </section>
+                <div class="wrapper">
+                   <div class="content">
+                       <jdoc:include type="modules" name="breadcrumbs" style="gangnam" />
+                    </div>
+                </div>
             </div>
         <?php endif; ?>
 
         <?php if($this->countModules('above')) : ?>
             <div class="above-row">
-                <section>
-                   <jdoc:include type="modules" name="above" style="gangnam" />
-                </section>
+                <div class="wrapper">
+                    <div class="content">
+                       <jdoc:include type="modules" name="above" style="gangnam" />
+                    </div>
+                </div>
             </div>
         <?php endif; ?>
 
         <?php if($siteHome != 'home' or ($frontpage == 0)) : ?>
         <div class="main-row">
-            <section>
-                <main role="main">
+            <div class="wrapper">
+                <main role="main" class="content">
                     <jdoc:include type="message" />
                     <jdoc:include type="component" />
                 </main>
@@ -74,21 +83,24 @@ include_once JPATH_THEMES . '/' . $this->template . '/logic.php';
                     <jdoc:include type="modules" name="complementary" style="gangnam" />
                 </div>
                 <?php endif; ?>
-            </section>
+            </div>
         </div>
         <?php endif; ?>
 
         <?php if($this->countModules('below')) : ?>
             <div class="below-row">
-                <section>
+                <div class="wrapper">
+                    <div class="content">
                     <jdoc:include type="modules" name="below" style="gangnam" />
-                </section>
+                    </div>
+                </div>
             </div>
         <?php endif; ?>
 
         <?php if($this->countModules('footer-menu') OR $this->countModules('contentinfo')) : ?>
             <div class="footer-row">
-                    <footer role="contentinfo">
+                <div class="wrapper">
+                    <footer role="contentinfo" class="content">
                         <?php if($this->countModules('footer-menu')) : ?>
                             <nav class="footer-menu">
                                 <jdoc:include type="modules" name="footer-menu" style="gangnam" />
@@ -99,33 +111,36 @@ include_once JPATH_THEMES . '/' . $this->template . '/logic.php';
                         <?php endif; ?>
                     </footer>
                 </div>
+            </div>
         <?php endif; ?>
 
         <div class="credit-row">
-            <footer>
-                <?php if ($social > 0) : ?>
-                    <ul class="social">
-                        <?php if ($twitterLink != "") : ?>
-                            <li><a class="twitter" href="<?php echo ($twitterLink); ?>" title="Follow me on Twitter" target="_blank"><?php echo ($twitter); ?></a></li>
-                        <?php endif; ?>
-                        <?php if ($dribbbleLink != "") : ?>
-                            <li><a class="dribbble" href="<?php echo ($dribbbleLink); ?>" title="See my latest work at Dribbble" target="_blank"><?php echo ($dribbble); ?></a></li>
-                        <?php endif; ?>
-                        <?php if ($facebookLink != "") : ?>
-                            <li><a class="facebook" href="<?php echo ($facebookLink); ?>" title="Annoy me on Facebook" target="_blank"><?php echo ($facebook); ?></a></li>
-                        <?php endif; ?>
-                        <?php if ($googleplusLink != "") : ?>
-                            <li><a class="googleplus" href="<?php echo ($googleplusLink); ?>" title="Find me on G+" target="_blank"><?php echo ($tgoogleplus); ?></a></li>
-                        <?php endif; ?>
-                        <?php if ($githubLink != "") : ?>
-                            <li><a class="github" href="<?php echo ($githubLink); ?>" title="All the codez" target="_blank"><?php echo ($github); ?></a></li>
-                        <?php endif; ?>
-                    </ul>
-                <?php endif; ?>
-                <div class="copyright">
-                    <small>&copy; <?php echo date("Y"); ?> <?php echo htmlspecialchars($app->getCfg('sitename'));?></small>
-                </div>
-            </footer>
+            <div class="wrapper">
+                <footer>
+                    <?php if ($social > 0) : ?>
+                        <ul class="social">
+                            <?php if ($twitterLink != "") : ?>
+                                <li><a class="twitter" href="<?php echo ($twitterLink); ?>" title="Follow me on Twitter" target="_blank"><?php echo ($twitter); ?></a></li>
+                            <?php endif; ?>
+                            <?php if ($dribbbleLink != "") : ?>
+                                <li><a class="dribbble" href="<?php echo ($dribbbleLink); ?>" title="See my latest work at Dribbble" target="_blank"><?php echo ($dribbble); ?></a></li>
+                            <?php endif; ?>
+                            <?php if ($facebookLink != "") : ?>
+                                <li><a class="facebook" href="<?php echo ($facebookLink); ?>" title="Annoy me on Facebook" target="_blank"><?php echo ($facebook); ?></a></li>
+                            <?php endif; ?>
+                            <?php if ($googleplusLink != "") : ?>
+                                <li><a class="googleplus" href="<?php echo ($googleplusLink); ?>" title="Find me on G+" target="_blank"><?php echo ($googleplus); ?></a></li>
+                            <?php endif; ?>
+                            <?php if ($githubLink != "") : ?>
+                                <li><a class="github" href="<?php echo ($githubLink); ?>" title="All the codez" target="_blank"><?php echo ($github); ?></a></li>
+                            <?php endif; ?>
+                        </ul>
+                    <?php endif; ?>
+                    <div class="copyright">
+                        <small>&copy; <?php echo date("Y"); ?> <?php echo htmlspecialchars($app->getCfg('sitename'));?></small>
+                    </div>
+                </footer>
+            </div>
         </div>
 
         <?php if($this->countModules('debug')) : ?>
